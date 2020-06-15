af2343283feb470e5b8c5eba5299034cafd6784a
Jeremy <qdhcjv@gmail.com>
2020-06-15 14:52:43 -0400

    Messing with new activities

--- app/src/main/java/me/jeremyfreedman/simpleap/MainActivity.kt
+++ app/src/main/java/me/jeremyfreedman/simpleap/MainActivity.kt
@@ -2,14 +2,15 @@ package me.jeremyfreedman.simpleap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}
+    val newRecipeFab = findViewById(R.id.floatingActionButton) as FloatingActionButton
+    newRecipeFab.setOnClickListener toNewRecipeActivity() {
 
-class NewStep : AppCompatActivity() {
-
+    }
 }
---
+++ app/src/main/java/me/jeremyfreedman/simpleap/NewRecipeActivity.kt
@@ -0,0 +1,11 @@
+package me.jeremyfreedman.simpleap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
